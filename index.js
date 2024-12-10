b1ee88e8fd21b9ccc547805c0a5a77014b7d9d75
laflopet <laflopet@gmail.com>
2024-12-09 22:54:30 -0500

    [ADD] insert file js

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const boton = document.getElementById('miBoton')
+
+function cambiar_color() {
+    boton.style.backgroundColor = "red";
+    boton.style.borderRadius = "10px"
+    boton.style.color = "white"
+    boton.style.padding = "10px"
+    alert("Cambiamos colores")
+};
+
